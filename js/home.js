a32e7dff60c0cd24d504e2053386e7ffc4084ca9
Mohammadreza Alinaghi <82723577+mohammadrezaAlinaghi@users.noreply.github.com>
2021-06-09 00:20:32 +0430

    V1

---
+++ js/home.js
@@ -0,0 +1,55 @@
+const clockElem     = document.getElementById('clock'),
+      digitalElem   = document.getElementById('digital'),
+      walledElem   = document.getElementById('walled');
+const digHourElem   = digitalElem.querySelector('#hour'),
+      digMinElem    = digitalElem.querySelector('#min'),
+      digSecondElem = digitalElem.querySelector('#second');
+const walHourElem   = walledElem.querySelector('#w-hour'),
+      walMinElem    = walledElem.querySelector('#w-min'),
+      walSecondElem = walledElem.querySelector('#w-second');
+( function () {
+    let digitalTimer = {
+        init: function(){
+            this.clocker();
+            this.timeInterval = this.runInterval(); 
+        },
+        runInterval: function(){
+             return setInterval(this.clocker, 1000);
+        },
+        clocker: function () {
+            let dateTime = new Date(),
+                hours    = dateTime.getHours(),
+                mins     = dateTime.getMinutes(),
+                seconds  = dateTime.getSeconds();
+            digHourElem.innerHTML   = (hours+'').padStart(2,0);
+            digMinElem.innerHTML    = (mins+'').padStart(2,0);
+            digSecondElem.innerHTML = (seconds+'').padStart(2,0);
+        },
+    }
+    digitalTimer.init();
+})();
+( function () {
+    let walledTimer = {
+        init: function(){
+            this.clocker();
+            this.timeInterval = this.runInterval(); 
+        },
+        runInterval: function(){
+             return setInterval(this.clocker, 1000);
+        },
+        clocker: function () {
+            let dateTime = new Date(),
+                hours    = dateTime.getHours(),
+                mins     = dateTime.getMinutes(),
+                seconds  = dateTime.getSeconds();
+            let sec_degree = -90 + seconds * 6;
+            let min_degree = -90 + mins * (360 / 60);
+            let hour_degree = -90 + hours * (360 / 12);
+            hour_degree = hour_degree + 0.5 * mins;
+            walHourElem.style.transform   = "rotate(" + hour_degree + "deg)";
+            walMinElem.style.transform = "rotate(" + min_degree + "deg)";
+            walSecondElem.style.transform = "rotate(" + sec_degree + "deg)";
+        },
+    }
+    walledTimer.init();
+})();
