0273dcbe03ddfda03d168cae966f604278017360
Mohammadreza Alinaghi <82723577+mohammadrezaAlinaghi@users.noreply.github.com>
2021-06-09 00:37:09 +0430

    V1.1.0

--- js/home.js
+++ js/home.js
@@ -1,12 +1,17 @@
+// Main containers
 const clockElem     = document.getElementById('clock'),
       digitalElem   = document.getElementById('digital'),
-      walledElem   = document.getElementById('walled');
+      walledElem    = document.getElementById('walled');
+// Digital Clock inner elements
 const digHourElem   = digitalElem.querySelector('#hour'),
       digMinElem    = digitalElem.querySelector('#min'),
       digSecondElem = digitalElem.querySelector('#second');
+// Walled Clock inner elements
 const walHourElem   = walledElem.querySelector('#w-hour'),
       walMinElem    = walledElem.querySelector('#w-min'),
       walSecondElem = walledElem.querySelector('#w-second');
+
+// Run DigitalClock
 ( function () {
     let digitalTimer = {
         init: function(){
@@ -28,6 +33,8 @@ const walHourElem   = walledElem.querySelector('#w-hour'),
     }
     digitalTimer.init();
 })();
+
+// Run WalledClock
 ( function () {
     let walledTimer = {
         init: function(){
@@ -42,12 +49,12 @@ const walHourElem   = walledElem.querySelector('#w-hour'),
                 hours    = dateTime.getHours(),
                 mins     = dateTime.getMinutes(),
                 seconds  = dateTime.getSeconds();
-            let sec_degree = -90 + seconds * 6;
-            let min_degree = -90 + mins * (360 / 60);
+            let sec_degree  = -90 + seconds * 6;
+            let min_degree  = -90 + mins * (360 / 60);
             let hour_degree = -90 + hours * (360 / 12);
-            hour_degree = hour_degree + 0.5 * mins;
+                hour_degree = hour_degree + 0.5 * mins;
             walHourElem.style.transform   = "rotate(" + hour_degree + "deg)";
-            walMinElem.style.transform = "rotate(" + min_degree + "deg)";
+            walMinElem.style.transform    = "rotate(" + min_degree + "deg)";
             walSecondElem.style.transform = "rotate(" + sec_degree + "deg)";
         },
     }
